c9c3417c0623aabd9299afa50e1c4bb0c3d037dc
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-22 13:29:43 -0300

    PUT - Melhorando designer do compenent services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
-
+  services = [
+    {
+      icon: '/powerbi.png',
+      title: 'Power BI Pro',
+      description: 'Crie dashboards incríveis e transforme dados em decisões estratégicas.'
+    },
+    {
+      icon: '/excel.png',
+      title: 'Excel Avançado',
+      description: 'Automatize processos e domine análises complexas com Excel.'
+    },
+    {
+      icon: '/data.png',
+      title: 'Consultoria de Dados',
+      description: 'Soluções personalizadas para o seu negócio crescer com dados.'
+    }
+  ];
 }
