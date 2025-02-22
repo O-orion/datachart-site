29de20201a3075d718e67cdc18152f7461d428a5
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-22 20:33:50 -0300

    PUT - Ajustando component de treinamento

--- src/app/components/treinamentos-carrousel/treinamentos-carrousel.component.ts
+++ src/app/components/treinamentos-carrousel/treinamentos-carrousel.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-treinamentos-carrousel',
   standalone: false,
   templateUrl: './treinamentos-carrousel.component.html',
-  styleUrl: './treinamentos-carrousel.component.scss'
+  styleUrl: './treinamentos-carrousel.component.scss',
+  animations: [
+    trigger('slideAnimation', [
+      state('void', style({ opacity: 0, transform: 'translateX(100%)' })),
+      state('*', style({ opacity: 1, transform: 'translateX(0%)' })),
+      transition(':enter', animate('0.6s cubic-bezier(0.4, 0, 0.2, 1)')),
+      transition(':leave', animate('0.6s cubic-bezier(0.4, 0, 0.2, 1)', style({ opacity: 0, transform: 'translateX(-100%)' })))
+    ])
+  ]
 })
 export class TreinamentosCarrouselComponent {
   trainings = [
@@ -56,12 +65,13 @@ export class TreinamentosCarrouselComponent {
   ];
 
   currentIndex = 0;
+
   prevSlide() {
-    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.trainings.length - 1;
+    this.currentIndex = this.currentIndex > 0 ? this.currentIndex - 1 : this.trainings.length - 1;
   }
 
   nextSlide() {
-    this.currentIndex = (this.currentIndex < this.trainings.length - 1) ? this.currentIndex + 1 : 0;
+    this.currentIndex = this.currentIndex < this.trainings.length - 1 ? this.currentIndex + 1 : 0;
   }
 
   goToSlide(index: number) {
