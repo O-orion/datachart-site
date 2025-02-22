918464ce892d0a59cb1db6ffba9cfe14fb3ad9c6
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-22 15:52:29 -0300

    PUT - Criando e ajustando component carousel de treinamentos

--- src/app/components/treinamentos-carrousel/treinamentos-carrousel.component.ts
+++ src/app/components/treinamentos-carrousel/treinamentos-carrousel.component.ts
@@ -7,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrl: './treinamentos-carrousel.component.scss'
 })
 export class TreinamentosCarrouselComponent {
+  trainings = [
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Power BI',
+      description: 'Capacitação prática para criar dashboards interativos.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Excel Avançado',
+      description: 'Domine fórmulas e automações para análises poderosas.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Workshop de Dados',
+      description: 'Estratégias para transformar dados em decisões.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Power BI',
+      description: 'Capacitação prática para criar dashboards interativos.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Excel Avançado',
+      description: 'Domine fórmulas e automações para análises poderosas.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Workshop de Dados',
+      description: 'Estratégias para transformar dados em decisões.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Power BI',
+      description: 'Capacitação prática para criar dashboards interativos.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Treinamento Excel Avançado',
+      description: 'Domine fórmulas e automações para análises poderosas.'
+    },
+    {
+      image: '/equipe-data.png',
+      title: 'Workshop de Dados',
+      description: 'Estratégias para transformar dados em decisões.'
+    }
+  ];
 
+  currentIndex = 0;
+  prevSlide() {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.trainings.length - 1;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex < this.trainings.length - 1) ? this.currentIndex + 1 : 0;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
 }
+
